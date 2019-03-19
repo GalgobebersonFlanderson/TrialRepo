44cc97e823611d2b1b2e5dc8d8e5974370a23fdd
Kirill Zhdakh <kzhdakh@fullsail.edu>
2019-03-19 13:15:40 -0400

    Initial commit

---
+++ TrialMavProj/TrialMavProj/src/main/java/trial_mav_proj/TrialMavProj/App.java
@@ -0,0 +1,13 @@
+package trial_mav_proj.TrialMavProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
